d7d64b5d3f20b021169e690191eac6e1669e0142
B22-User <fedbidtest1@gmail.com>
2021-05-25 19:23:36 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,7 +1,13 @@
 public class BranchingPractice {
 
     public static void main(String[] args) {
-        
+
+        // created new project
+        // enabled version control
+        // added .gitnore with .idea , target folder and .iml file
+        // made initial commit by keeping track of all files
+        // shared project on github
+
     }
 
 }
